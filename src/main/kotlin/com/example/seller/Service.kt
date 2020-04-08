845f43646bf8b1e3a06e271944531ec8ae6a2934
Luram Archanjo <luram.archanjo@sensedia.com>
2020-04-08 18:42:27 -0300

    Cloudevents

--- src/main/kotlin/com/example/seller/Service.kt
+++ src/main/kotlin/com/example/seller/Service.kt
@@ -1,15 +1,21 @@
 package com.example.seller
 
+import io.cloudevents.v03.CloudEventBuilder
+import io.cloudevents.v03.CloudEventImpl
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.net.URI
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class Service(
-        val kafkaTemplate: KafkaTemplate<String, Advertisement>,
+        val kafkaTemplate: KafkaTemplate<String, CloudEventImpl<Advertisement>>,
         @Value("\${spring.application.name}") val applicationName: String
 ) {
 
@@ -20,9 +26,16 @@ class Service(
     @Scheduled(initialDelay = 1000, fixedDelay = 3000)
     fun producer() {
         val advertisement = Advertisement(id = counter.getAndIncrement().toString(), name = applicationName)
+        val cloudEvent: CloudEventImpl<Advertisement> = CloudEventBuilder.builder<Advertisement>()
+                .withId(UUID.randomUUID().toString())
+                .withSource(URI.create("/advertisements/${advertisement.id}"))
+                .withType("Advertisement")
+                .withTime(ZonedDateTime.now(ZoneOffset.UTC))
+                .withData(advertisement)
+                .build()
 
-        log.info("Publishing Message=[$advertisement] to Topic=[${kafkaTemplate.defaultTopic}]")
-        kafkaTemplate.sendDefault(applicationName, advertisement)
+        log.info("Publishing Message=[$cloudEvent] to Topic=[${kafkaTemplate.defaultTopic}]")
+        kafkaTemplate.sendDefault(applicationName, cloudEvent)
     }
 
 }
